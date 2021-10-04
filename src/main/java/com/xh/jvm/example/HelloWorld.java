03ad9dc1ba92704697d38f4f93616bd9542e0830
sgj <879776225@qq.com>
2021-10-04 16:30:18 +0800

    测试类

---
+++ src/main/java/com/xh/jvm/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.xh.jvm.example;
+
+/**
+ * Created By xh
+ * 2021/7/6
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
